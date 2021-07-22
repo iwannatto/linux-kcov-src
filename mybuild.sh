9347f70bdb786e24ca0362f00d8dfc11324ad611
iwannatto <iwannatto@gmail.com>
2021-07-22 22:41:31 +0900

    Limit instrumentation to kcov-related part and Add documentation

--- mybuild.sh
+++ mybuild.sh
@@ -1,1 +1,3 @@
-make-kpkg --rootcmd fakeroot --append-to-version -kcov --revision 0.2 --initrd --jobs 8 kernel_image kernel_headers
+set -eux
+
+make-kpkg --rootcmd fakeroot --append-to-version -kcov --revision 0.6 --initrd --jobs 8 kernel_image kernel_headers
